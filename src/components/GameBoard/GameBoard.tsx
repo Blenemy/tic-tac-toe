2e256ebe8a4f3079b319ac24d773f14f79516f85
Denys Topchyi <miliarder97@gmail.com>
2024-05-04 17:24:25 +0300

    feat: add basic styles to the board

--- src/components/GameBoard/GameBoard.tsx
+++ src/components/GameBoard/GameBoard.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
+
+import GameField from "../GameField/GameField";
+
 import "./GameBoard.scss";
 
+const NUMBER_OF_FIELDS = 9;
+
 const GameBoard = () => {
+  const [gameFields, setGameFields] = useState<boolean[]>([]);
+
+  useEffect(() => {
+    const newArray = Array(NUMBER_OF_FIELDS).fill(false);
+    setGameFields(newArray);
+  }, []);
+
+  const handleCellClick = (i: number) => {
+    const modifiedArray = gameFields.map((item, index) =>
+      index === i ? true : item
+    );
+
+    setGameFields(modifiedArray);
+  };
+
   return (
-    <div>
-      <div>GameBoard</div>
+    <div className="gameboard">
+      {gameFields.map((cellState, index) => (
+        <GameField
+          key={index}
+          onCellClick={handleCellClick}
+          index={index}
+          cellState={cellState}
+        />
+      ))}
     </div>
   );
 };
