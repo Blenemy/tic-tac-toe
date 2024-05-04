4a9029fcd2d035276894a85399d72527adf0ddd4
Denys Topchyi <miliarder97@gmail.com>
2024-05-04 16:35:05 +0300

    feat: setup basic components, add reset scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+import GameBoard from "./components/GameBoard/GameBoard";
+
+import "./styles/_reset.scss";
 
 function App() {
+  const [gameStarted, setGameStarted] = useState<boolean>(false);
+
+  const handleStartGame = () => {
+    setGameStarted(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__container">
+        <div className="app__content">
+          <div className="gamefield">
+            <button
+              onClick={handleStartGame}
+              className="gamefield__start-button"
+            >
+              Start the game
+            </button>
+            {gameStarted ? <GameBoard /> : <div>123</div>}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/GameBoard/GameBoard.tsx
@@ -0,0 +1,11 @@
+import "./GameBoard.scss";
+
+const GameBoard = () => {
+  return (
+    <div>
+      <div>GameBoard</div>
+    </div>
+  );
+};
+
+export default GameBoard;
